9e7162aab8406b43e1e7238e3ac78fdd53c31395
sajadanjidani <sajadanjiidanii@gmail.com>
2025-03-23 16:03:28 +0330

    create : custome-element folder and file

---
+++ public/component/custome-element/custome.js
@@ -0,0 +1,7 @@
+const $ = document
+
+import { HeaderNavbar } from "../header-navbar/header-navbar.js";
+window.customElements.define("header-navbar",HeaderNavbar)
+
+import { FooterItem } from "../footer-item/footer-item.js";
+window.customElements.define("footer-item",FooterItem)
